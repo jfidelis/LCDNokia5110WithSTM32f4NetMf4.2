acbddb6b159449c96135f817cb29e4953376c729
Jeferson IP Fidelis <jefersonfidelis@terra.com.br>
2016-10-03 08:43:34 -0300

    Teste de entrega ao git

---
+++ LCDNokia5110/LCDNokia5110/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.SPOT;
+
+namespace LCDNokia5110
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+            // Teste do git...
+        }
+    }
+}
